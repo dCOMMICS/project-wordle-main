d02b2bf40125d455f305d8f0719638cffef53024
dCOMMICS <sanwurld@gmail.com>
2024-04-06 20:57:13 +0300

    🤲📿🚀

--- fundamentals/index.js
+++ fundamentals/index.js
@@ -1,26 +1,34 @@
-function render(reactElement, containerDOMElement) {
-    // 1. Create a DOM element
-    const domElement = document.createElement (reactElement.type)
+// function render(reactElement, containerDOMElement) {
+//     // 1. Create a DOM element
+//     const domElement = document.createElement (reactElement.type)
 
-    // 2. update properties
+//     // 2. update properties
 
-    domElement.innerText = reactElement.children;
+//     domElement.innerText = reactElement.children;
 
-    // 3. put it in the container
+//     // 3. put it in the container
 
-    containerDOMElement.appendChild (domElement); 
+//     containerDOMElement.appendChild (domElement); 
 
     
+// }
+
+// const reactElement = {
+//     type : 'a',
+//     props: {    
+//         href: 'http://yahoo.com/', 
+//     },
+//     children: 'Read more about this component',
+// };
+
+// const containerDOMElement = document.querySelector ('root');
+
+// render (reactElement, containerDOMElement);
+
+
+function App () {
+    return React.createElement('header', null, "My Name Is IBRAHIM KAYODE");
 }
 
-const reactElement = {
-    type : 'a',
-    props: {    
-        href: 'http://yahoo.com/', 
-    },
-    children: 'Read more about this component',
-};
-
-const containerDOMElement = document.querySelector ('root');
-
-render (reactElement, containerDOMElement);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root,render (React.createElement(App))
--- fundamentals/readme.md
+++ fundamentals/readme.md
@@ -3,3 +3,6 @@ react is a js library for building user interfaces (UI)
 it all about components.
 react is declarative. JSX (A SYNTAX THAT COMBINES HTML CSS JAVASCRIPT as well as referencing other components).
  
+
+
+ <!-- boat rides and hire website  -->
