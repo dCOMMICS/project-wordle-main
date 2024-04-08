2cc6e91f639fb99ee535a1345bd83260321286b3
dCOMMICS <sanwurld@gmail.com>
2024-04-08 19:05:51 +0300

    working on errors

--- website/ev-website/src/components/background/bcg.jsx
+++ website/ev-website/src/components/background/bcg.jsx
@@ -2,18 +2,26 @@
 import './bcg.css';
 import video1 from '../../assets/video1.mp4';
 import image1 from '../../assets/image1.png';
-const bcg = () => {
+import image2 from '../../assets/image2.png';
+import image3 from '../../assets/image3``.png';
+
+
 
     const background= ({playStatus, heroCount}) => {
 
-    
-  return (
-    <div>
+    if (playStatus) {
+        return (
+            <video className='background' autoPlay loop muted>
+                <source src={video1} type='video/mp4' />
+            </video>
+        )
+    }
 
-
-
-    </div>
-  )
+    else if (heroCount===0)
+    {
+        return <img src={image1} className='background' alt='pic suv left' />
+    }
+  
 }
 
 export default bcg
