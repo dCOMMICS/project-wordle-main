c5c91b6e539388abc2a8ad9f7a2442c5bff1fc01
dCOMMICS <sanwurld@gmail.com>
2024-04-08 18:32:34 +0300

    bombarded with a few error....

--- website/ev-website/src/components/background/bcg.jsx
+++ website/ev-website/src/components/background/bcg.jsx
@@ -1,6 +1,12 @@
 
-import './bcg.css'
+import './bcg.css';
+import video1 from '../../assets/video1.mp4';
+import image1 from '../../assets/image1.png';
 const bcg = () => {
+
+    const background= ({playStatus, heroCount}) => {
+
+    
   return (
     <div>
 
