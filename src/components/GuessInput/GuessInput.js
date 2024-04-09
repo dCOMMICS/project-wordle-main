a9f202f8ad487f2f2d66a1c4192b21d220dad438
dCOMMICS <sanwurld@gmail.com>
2024-04-09 23:59:01 +0300

    init commit

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,28 +1,61 @@
-import React from 'react';
+// import React from 'react';
+
+// function GuessInput() {
+//   const [guess, setGuess] = React.useEffect('');
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+
+//     console.log({ guess })
+
+//     setGuess('');
+//   }
+//   return (
+//     <form className="guess-input-wrapper" >
+//       <label htmlFor="guess-input">Enter guess:</label>
+//       <input value={guess} onChange={(event) => {
+
+//         const nextGuess = event.target.value.toUpperCase();
+//         setGuess(nextGuess);
+
+
+//         setGuess(event.target.value.toUpperCase());
+//       }} id='guess-input' type='text'></input>
+//     </form>
+//   )
+// }
+
+// export default GuessInput;
+
+import React, { useState } from 'react';
 
 function GuessInput() {
-  const [guess, setGuess] = React.useEffect('');
+  const [guess, setGuess] = useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log({ guess })
+    console.log({ guess });
 
     setGuess('');
   }
+
   return (
-    <form className="guess-input-wrapper" >
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input value={guess} onChange={(event) => {
-
-        const nextGuess = event.target.value.toUpperCase();
-        setGuess(nextGuess);
-
-
-        setGuess(event.target.value.toUpperCase());
-      }} id='guess-input' type='text'></input>
+      <input
+        value={guess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
+        id="guess-input"
+        type="text"
+      />
     </form>
-  )
+  );
 }
 
 export default GuessInput;
+
+
