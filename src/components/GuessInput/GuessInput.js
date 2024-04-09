c1ef26f73baa945841b7c2087fec90f1131fb0b9
dCOMMICS <sanwurld@gmail.com>
2024-04-09 23:46:09 +0300

    💯🚀

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,11 +2,24 @@ import React from 'react';
 
 function GuessInput() {
   const [guess, setGuess] = React.useEffect('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({ guess })
+
+    setGuess('');
+  }
   return (
-    <form className = "guess-input-wrapper" >
+    <form className="guess-input-wrapper" >
       <label htmlFor="guess-input">Enter guess:</label>
-      <input value={guess} onChange={(event) =>{
-        setGuess(event.target.value);
+      <input value={guess} onChange={(event) => {
+
+        const nextGuess = event.target.value.toUpperCase();
+        setGuess(nextGuess);
+
+
+        setGuess(event.target.value.toUpperCase());
       }} id='guess-input' type='text'></input>
     </form>
   )
