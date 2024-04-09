66ee87d551350dda8dfdaa09091d33b246d9d8a9
dCOMMICS <sanwurld@gmail.com>
2024-04-09 23:38:12 +0300

    wordle project url fixing

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 function GuessInput() {
+  const [guess, setGuess] = React.useEffect('');
   return (
     <form className = "guess-input-wrapper" >
       <label htmlFor="guess-input">Enter guess:</label>
-      <input id='guess-input' type='text'></input>
+      <input value={guess} onChange={(event) =>{
+        setGuess(event.target.value);
+      }} id='guess-input' type='text'></input>
     </form>
   )
 }
